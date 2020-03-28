5c26398f686107e038a411960c0166e0f11e8408
Konyaev Maksim <redsun1988@rambler.ru>
2020-03-29 01:14:48 +0300

    new bad comments were added

--- dbManager.py
+++ dbManager.py
@@ -15,18 +15,47 @@ class dbManager:
             cursor.execute(query)
             return cursor.fetchall()
 
+    def getPageOfComments(self, skip, take):
+        query = self.generatePaginatedQuery(skip, take)
+        
+        conn = pyodbc.connect(self.connectionString)
+        with conn:
+            cursor = conn.cursor().execute(query)
+            cursor.execute(query)
+            return cursor.fetchall()
+
     def generateBasicQuery(self):
         query = """
-        SELECT DISTINCT TOP 1000 [Text] as [text] 
+        SELECT DISTINCT TOP 500 [Text] as [text] 
 	    ,[Tickets].[FriendlyId] as [id]
         FROM [SupportCenterPaid].[c1f0951c-3885-44cf-accb-1a390f34c342].[Posts]
         INNER JOIN [SupportCenterPaid].[c1f0951c-3885-44cf-accb-1a390f34c342].[Tickets] 
 	        on [Tickets].[Id] = [Posts].[Ticket_Id] 
         INNER JOIN [SupportCenterPaid].[c1f0951c-3885-44cf-accb-1a390f34c342].[Users] 
-	        on [Users].[Id] = [Posts].[Owner] 
-        WHERE 
+	        on [Users].[Id] = [Posts].[Owner]
+
+        WHERE
 		[Posts].[Modified] > CAST(CURRENT_TIMESTAMP AS DATE)
 		and
 		[Users].[IsEmployee] = 0
         """
+        return query
+
+    def generatePaginatedQuery(self, skip, take):
+        query = """
+        SELECT [Text] as [text] 
+	    ,[Tickets].[FriendlyId] as [id]
+        FROM [SupportCenterPaid].[c1f0951c-3885-44cf-accb-1a390f34c342].[Posts]
+        INNER JOIN [SupportCenterPaid].[c1f0951c-3885-44cf-accb-1a390f34c342].[Tickets] 
+	        on [Tickets].[Id] = [Posts].[Ticket_Id] 
+        INNER JOIN [SupportCenterPaid].[c1f0951c-3885-44cf-accb-1a390f34c342].[Users] 
+	        on [Users].[Id] = [Posts].[Owner]
+
+        WHERE
+		[Users].[IsEmployee] = 0
+
+        ORDER BY [Posts].[Modified]
+        OFFSET %d ROWS
+        FETCH NEXT %d ROWS ONLY
+        """ % (skip, take)
         return query
--- main.py
+++ main.py
@@ -18,13 +18,8 @@ def index_page():
 
 @app.route("/checkSubject", methods=["POST"])
 def checkSubject():
-    if request.method == "POST":
-        text = request.json["text"]
-        
-        fitness = classifier.predict_text(text)
-        print("Text - ", text, " result - " + fitness)
-
-    return json.jsonify({ "text": text, "fitness": fitness, "message": message })
+    text = request.get_json(force=True)["text"]
+    return json.jsonify({ "text": text, "class": classifier.predict_text(text),"confidence": classifier.predict_proba(text)})
 
 @app.route("/getTodayComments", methods=["POST", "GET"])
 def getTodayComments():
@@ -34,19 +29,53 @@ def getTodayComments():
         print("A sql query executueting throws an error in getTodayComments")
         print(ex)
         return abort(500) 
-    
-    print("getTodayComments")
+
     estimatedComments = [{ 
         "text": i[0], 
         "id": i[1], 
-        "fitness": classifier.predict_text(i[0]) 
-    } for i in data]
+        "class": classifier.predict_text(i[0]),
+        "confidence": classifier.predict_proba(i[0]),
+        "text_len": len(i[0]) 
+    } for i in data if i[0] is not None]
+
+    return json.jsonify(estimatedComments)
+
+@app.route("/getWorseThen", methods=["POST", "GET"])
+def getWorseThen():
+    estimatedComments = []
+    skip = 0
+    take = 200
+    while len(estimatedComments) < 200:
+        try:
+            print("try getWorseThen")
+            data = dbManager.getPageOfComments(skip, take)
+        except Exception as ex:
+            print("A sql query executueting throws an error in getWorseThen")
+            print(ex)
+            return abort(500) 
+
+        temp = [{ 
+            "text": i[0], 
+            "id": i[1], 
+            "class": classifier.predict_text(i[0]),
+            "confidence": classifier.predict_proba(i[0]),
+            "text_len": len(i[0]) 
+        } for i in data if i[0] is not None 
+        and 
+        classifier.predict_proba(i[0]) > 0.75 
+        and 
+        classifier.predict_text(i[0]) == -1]
+
+        estimatedComments += temp
+
+        print(skip, take, len(estimatedComments))
+        skip += take
 
     return json.jsonify(estimatedComments)
 
 if __name__ == "__main__":
     '''
-    app.run(debug=True)
+    app.run(port=5006, debug=True)
     '''
     from waitress import serve
-    serve(app, port=5006, url_scheme='https')
+    serve(app, port=5006)
